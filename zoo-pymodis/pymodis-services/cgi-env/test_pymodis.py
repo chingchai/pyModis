b4676764e3a069a09df713ea4aaef95442845f75
chingchai <chingchai.h@gmail.com>
2016-06-23 00:33:46 +0700

    rearrange code test_pymodis service

--- zoo-pymodis/pymodis-services/cgi-env/test_pymodis.py
+++ zoo-pymodis/pymodis-services/cgi-env/test_pymodis.py
@@ -1,5 +1,7 @@
 import zoo
 def test_pymodis(conf,inputs,outputs):
     outputs["Result"]["value"]=\
-            " | dns: "+inputs["dns"]["value"]+ " | pwd: "+inputs["pwd"]["value"]+ " | usr: "+inputs["usr"]["value"]+ " | path:"+inputs["path"]["value"]+ " | product:" +inputs["product"]["value"]+ " | tiles:"+inputs["tiles"]["value"]+" | today:"+inputs["today"]["value"]+ " | enddate:"+inputs["enddate"]["value"]
+            " | dns: "+inputs["dns"]["value"]+ " | pwd: "+inputs["pwd"]["value"]+ " | usr: "+inputs["usr"]["value"]+\
+            " | path:"+inputs["path"]["value"]+ " | product:" +inputs["product"]["value"]+ " | tiles:"+inputs["tiles"]["value"]+\
+            " | today:"+inputs["today"]["value"]+ " | enddate:"+inputs["enddate"]["value"]
     return zoo.SERVICE_SUCCEEDED
